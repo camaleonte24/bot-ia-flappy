61202ac4ad1348851019d88a891b9761a6b6c162
camaleonte24 <atorinogiovanni100@gmail.com>
2026-09-22 19:34:01 +0200

    Remove GuildMembers intent from index.js

--- index.js
+++ index.js
@@ -170,8 +170,7 @@ const client = new Client({
         GatewayIntentBits.Guilds, 
         GatewayIntentBits.GuildMessages, 
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.DirectMessages
     ],
     partials: [Partials.Channel, Partials.Message]
 });
